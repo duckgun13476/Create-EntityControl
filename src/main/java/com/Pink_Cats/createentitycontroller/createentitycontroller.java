389d9205b70934ecbc4868981c36e7ed02b2ed7a
Pink_Cats <1907284584@qq.com>
2025-05-05 15:40:02 +0800

    Fix crush when running server.

--- src/main/java/com/Pink_Cats/createentitycontroller/createentitycontroller.java
+++ src/main/java/com/Pink_Cats/createentitycontroller/createentitycontroller.java
@@ -21,7 +21,7 @@ public class createentitycontroller {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "createentitycontroller";
     // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final Logger LOGGER = LogUtils.getLogger();
 
 
     public createentitycontroller() {
