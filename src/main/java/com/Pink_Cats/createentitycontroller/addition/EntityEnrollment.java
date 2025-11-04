78374487c8d85a01ded3ddb38ae08a57d81f6a43
Pink_Cats <1907284584@qq.com>
2025-11-04 13:13:46 +0800

    1. Fix code bugs to decrease calculate cost. 2. Fix debug problem.
    # Conflicts:
    #	build.gradle
    #	changes/1.20.1-0.2.6-0.5.1j.md
    #	changes/1.20.1-0.2.7-0.5.1j.md
    #	gradle.properties
    #	src/main/java/com/Pink_Cats/createentitycontroller/mixin/ContraptionMixin.java
    #	update/history/1.20.1-0.2.4-0.5.1j.md
    #	update/history/1.20.1-0.2.4.md
    #	update/history/1.20.1-0.2.5-0.5.1j.md
    #	update/history/1.20.1-0.2.6-0.5.1j.md
    #	update/history/1.20.1-0.2.6-6.0.6+.md
    #	update/history/1.20.1-0.2.7-0.5.1j.md
    #	update/history/1.20.1-0.2.7-6.0.+.md

--- src/main/java/com/Pink_Cats/createentitycontroller/addition/EntityEnrollment.java
+++ src/main/java/com/Pink_Cats/createentitycontroller/addition/EntityEnrollment.java
@@ -2,15 +2,12 @@ package com.Pink_Cats.createentitycontroller.addition;
 
 public class EntityEnrollment {
 
-        // 私有变量
         private static int ControlStatus = 0;
 
-        // Getter 方法
         public static int getControlStatus() {
             return ControlStatus;
         }
 
-        // Setter 方法
         public static void setControlStatus(int ControlStatus) {
             EntityEnrollment.ControlStatus = ControlStatus;
         }
