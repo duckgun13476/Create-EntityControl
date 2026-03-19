f89a79c87f0f72d61a241cf72e5b2d75ca0b74e2
Pink_Cats <1907284584@qq.com>
2026-03-19 21:42:46 +0800

    Fix old api.

--- src/main/java/com/Pink_Cats/createentitycontrol/CreateEntityControl.java
+++ src/main/java/com/Pink_Cats/createentitycontrol/CreateEntityControl.java
@@ -3,14 +3,11 @@ package com.Pink_Cats.createentitycontrol;
 import com.Pink_Cats.createentitycontrol.command.BlockifyContraptionCommand;
 import com.Pink_Cats.createentitycontrol.network.CreateEntityControlNetwork;
 import com.mojang.logging.LogUtils;
-import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.RegisterCommandsEvent;
@@ -60,14 +57,5 @@ public class CreateEntityControl {
     @SubscribeEvent
     public void onRegisterCommands(RegisterCommandsEvent event) {
         BlockifyContraptionCommand.register(event.getDispatcher());
-    }
-
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
-            // Some client setup code
-           }
     }
 }
